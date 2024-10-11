e62283136afcae8c14a5bba78a339e3fffd5b726
Joe Shymanski <joe-shymanski@utulsa.edu>
2024-10-11 18:48:12 -0500

    Added memory, restricted influencers to swap only with non-influencers, evenly or randomly distribute influencers at the start, etc.

--- simulation.py
+++ simulation.py
@@ -4,7 +4,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 from mesa import Agent, Model
-from mesa.time import BaseScheduler, SimultaneousActivation, RandomActivation
+from mesa.time import SimultaneousActivation, StagedActivation
 from mesa.space import SingleGrid, NetworkGrid
 from mesa.datacollection import DataCollector
 
@@ -33,20 +33,23 @@ def compute_adoption_ratio(model):
 
 # --- Agent Class ---
 class SocialAgent(Agent):
-    def __init__(self, unique_id, model, initial_state, influencer=False):
+    def __init__(self, unique_id, model, initial_state, influencer=False, memory=0):
         super().__init__(unique_id, model)
         self.state = initial_state  # Either 0 (non-adopter) or 1 (adopter)
         self.influencer = influencer
         self.mobility_rate = model.mobility_rate
+        self.memory = memory
+        self.past_states = []
 
     def step(self):
         # Influencers may move based on mobility rate
-        if random.random() < self.mobility_rate:
+        if self.influencer and random.random() < self.mobility_rate:
             self.move()
             ### Don't move into another influencer
             ### Influencers are eager to move to greener pastures after stagnation or reaching a certain threshold
             ### Those who get influenced can also move to a different location
             ### Influencer interaction rate can increase
+            ### Move only where you have not been before
 
         # Interact with neighbors (binary or n-ary interaction)
         if not self.influencer:
@@ -54,27 +57,30 @@ class SocialAgent(Agent):
             interaction_result = self.interact(neighbors)
 
             # Update state based on interactions
-            self.state = int(interaction_result > (len(neighbors) / 2))
+            self.state = round(interaction_result)
+
+            # Store current state in memory
+            self.past_states.append(self.state)
+
+            # Keep only the last M states in memory
+            if len(self.past_states) > self.memory:
+                self.past_states.pop(0)
 
     def interact(self, neighbors):
-        """Count how many neighbors are in the '1' state (adopted norm)"""
-        count = 0
-        for neighbor in neighbors:
-            if self.model.grid.is_cell_empty(neighbor.pos):
-                continue
-            neighbor_agent = self.model.grid.get_cell_list_contents([neighbor.pos])[0]
-            if neighbor_agent.state:
-                count += 1
-        return count
+        total_states = [n.state for n in neighbors]
+        return sum(total_states) / len(total_states)
 
     def move(self):
         """Move to a random neighboring position (von Neumann, Moore, etc.)"""
-        neighbor = random.choice(list(self.model.grid.get_neighbors(self.pos, moore=self.model.moore, include_center=False)))
-        self.model.grid.swap_pos(self, neighbor)
+        neighbors = self.model.grid.get_neighbors(self.pos, moore=self.model.moore, include_center=False)
+        non_influencer_neighbors = [n for n in neighbors if not n.influencer]
+        if non_influencer_neighbors:
+            neighbor = random.choice(neighbors)
+            self.model.grid.swap_pos(self, neighbor)
 
 # --- Model Class ---
 class SocialNormModel(Model):
-    def __init__(self, N, f, topology, moore, mobility_rate, interaction_type, influencer_placement, k_clumps=1):
+    def __init__(self, N, f, topology, moore, memory, mobility_rate, interaction_type, influencer_placement="even", k_clumps=1):
         super().__init__()
         self.num_agents = N
         self.num_influencers = int(f * N)
@@ -96,18 +102,39 @@ class SocialNormModel(Model):
         self.grid = SingleGrid(int(np.sqrt(N)), int(np.sqrt(N)), torus=False)
         self.schedule = SimultaneousActivation(self)
 
-        # Create agents
-        for i in range(N):
-            initial_state = 0 if i >= self.num_influencers else 1
-            influencer = i < self.num_influencers
-            agent = SocialAgent(i, self, initial_state, influencer)
+        if influencer_placement == "even":
+            influencer_indices = self.get_regular_intervals(N, self.num_influencers)
+        elif influencer_placement == "random":
+            influencer_indices = random.sample(range(N), self.num_influencers)
+        elif influencer_placement == "clumps":
+            influencer_indices = []
+            for i in range(k_clumps):
+                clump_size = self.num_influencers // k_clumps
+                clump = random.sample(range(N), clump_size)
+                influencer_indices.extend(clump)
+
+        for i in range(self.num_agents):
+            is_influencer = i in influencer_indices
+            agent = SocialAgent(i, self, int(is_influencer), is_influencer, memory)
             self.grid.place_agent(agent, (i // int(np.sqrt(N)), i % int(np.sqrt(N))))
             self.schedule.add(agent)
+
+        # # Create agents
+        # for i in range(N):
+        #     initial_state = 0 if i >= self.num_influencers else 1
+        #     influencer = i < self.num_influencers
+        #     agent = SocialAgent(i, self, initial_state, influencer, memory)
+        #     self.schedule.add(agent)
 
         # Data Collection
         self.datacollector = DataCollector(
             model_reporters={"Adoption Ratio": compute_adoption_ratio}
         )
+
+    def get_regular_intervals(self, total_agents, num_influencers):
+        """ Return indices of influencers spaced at regular intervals throughout the grid. """
+        interval = total_agents // num_influencers
+        return list(range(0, total_agents, interval))
 
     def step(self):
         self.datacollector.collect(self)
@@ -119,13 +146,15 @@ def run_simulation(
         f=.1,
         topology="lattice",
         moore=False,
+        memory=5,
         steps=100,
         mobility_rate=.1,
         interaction_type="binary",
-        influencer_placement="random",
+        influencer_placement="even",
+        k_clumps=1,
         show_grid=False
     ):
-    model = SocialNormModel(N, f, topology, moore, mobility_rate, interaction_type, influencer_placement)
+    model = SocialNormModel(N, f, topology, moore, memory, mobility_rate, interaction_type, influencer_placement, k_clumps)
 
     # Run the model
     for step in range(steps):
@@ -156,10 +185,13 @@ def run_simulation(
 # Example usage
 run_simulation(
     N=100,
-    f=.25,
+    f=.1,
     topology="lattice",
     moore=True,
+    memory=5,
     steps=1000,
-    mobility_rate=.05,
+    mobility_rate=.25,
+    influencer_placement="even",
+    k_clumps=5,
     show_grid=False
 )
